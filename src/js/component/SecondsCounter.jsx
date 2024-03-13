6c9f461d452577e398c576f82bc34dd2b4321031
Aelf86 <139387685+Aelf86@users.noreply.github.com>
2024-03-13 18:55:28 +0000

    mis ultimos cambios

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -5,7 +5,7 @@ import "../../styles/index.css"
 
 function SecondsCounter(props) {
 	return (<div className='bigCounter'>
-		<div className='d-flex align-items-center d-grid gap-1 fs-2 bg-white'>
+		<div className='d-flex align-items-center d-grid gap-1 fs-2 bg-dark'>
 			<i className="fa-regular fa-clock btn btn-dark py-2 fs-2"></i>
 			<div className="btn btn-dark py-2 border fs-2">{props.hoursTwo}</div>
 			<div className="btn btn-dark py-2 border fs-2">{props.hours}</div>
