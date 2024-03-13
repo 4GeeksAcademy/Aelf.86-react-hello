ddb285166e9b4d25a9c792ffc29d7bc2e8d0b472
Aelf86 <139387685+Aelf86@users.noreply.github.com>
2024-03-13 02:37:16 +0000

    mi tarea

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "../../styles/index.css"
+
+function SecondsCounter(props) {
+	return (<div className='bigCounter'>
+		<div className='d-flex align-items-center d-grid gap-1 fs-2 bg-white'>
+			<i className="fa-regular fa-clock btn btn-dark py-2 fs-2"></i>
+			<div className="btn btn-dark py-2 border fs-2">{props.hoursTwo}</div>
+			<div className="btn btn-dark py-2 border fs-2">{props.hours}</div>
+			<div className="btn btn-dark py-2 border fs-2">{props.minutesTwo}</div>
+			<div className="btn btn-dark py-2 border fs-2">{props.minutes}</div>
+			<div className="btn btn-dark py-2 border fs-2">{props.secondsTwo}</div>
+			<div className="btn btn-dark py-2 border fs-2">{props.seconds}</div>
+		</div>
+
+	</div>);
+
+}
+
+export default SecondsCounter
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,39 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import SecondsCounter from "./component/SecondsCounter.jsx";
+
+let seg, seg2, min, min2, hour, hour2;
+seg = seg2 = min = min2 = hour = hour2 = 0;
+
+setInterval(()=>{
+    if (seg == 9){
+        seg = -1
+        seg2++
+    }
+    if (seg2 == 6){
+        seg2 = 0
+        min++
+    }
+    if (min == 9){
+        min = 0
+        min2++
+    }
+    if (min2 == 6){
+        min2 = 0
+        hour++
+    }
+    if (hour == 9){
+        hour = 0
+        hour2++
+    }
+    if (hour == 4 && hour2 == 2 ){
+        seg = -1
+        seg2 = min = min2 = hour = hour2 = 0;
+    }
+    seg++
+    ReactDOM.render(<SecondsCounter seconds={seg} secondsTwo={seg2} minutes={min} minutesTwo={min2} hours={hour} hoursTwo={hour2}  />, document.querySelector("#app"));    
+}, 1000)
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
